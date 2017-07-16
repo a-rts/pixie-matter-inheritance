98bb4b7727cd88a4ab1fc9363e29b9b262f74f93
a-rts <a-rts@users.noreply.github.com>
2017-07-16 03:17:14 +0100

    Repackaged and moved babel-cli back to dev dependencies

--- server.js
+++ server.js
@@ -56,19 +56,3 @@ if (isDevelopemt) {
 app.listen(app.get('port'), function () {
     console.log('Serving ' + HTML_FILE + ' on port ' + app.get('port'));
 });
-
-// var express = require('express');
-// var app = express();
-// var path = require('path');
-//
-// var port = process.env.PORT || 8080;
-//
-// app.use('/', express.static('dist'))
-//
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname + '/dist/index.html'));
-// });
-//
-// app.listen(port, function() {
-//   console.log('Serving index.html on port ' + port);
-// });
